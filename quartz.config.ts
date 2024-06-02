85ed2c040093100e07e7a9824b982a9481396dd5
gitblanc <uo285176@uniovi.es>
2024-06-02 19:45:42 +0200

    update quartz baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Obsidian Notes💚💜",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gitblanc.github.io/Obsidian-Notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
