121c914bee7fc3572dd3e175c8e6cbd4def3ff01
gitblanc <uo285176@uniovi.es>
2024-06-03 22:01:08 +0200

    añadidos comentarios, buy me a coffee y los útlimos retoques estéticos de la página

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,6 +14,16 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
+        <h2 id="how-is-this-page">Dudas y comentarios</h2>
+        <script
+          src="https://utteranc.es/client.js"
+          repo="gitblanc/Obsidian-Notes"
+          issue-term="pathname"
+          label="Comments"
+          theme="github-dark"
+          crossorigin="anonymous"
+          async
+        ></script>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
@@ -25,6 +35,18 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="gitblanc"
+          data-description="Support me on Buy me a coffee!"
+          data-message=""
+          data-color="#8000ff"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        ></script>
       </footer>
     )
   }
