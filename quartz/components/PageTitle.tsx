50b604a4cbcbda9ff035d4f8a41d8e7af7b900f6
Eduardo Blanco Bielsa <gitblanc@tutanota.com>
2024-08-29 11:45:20 +0200

    added logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
