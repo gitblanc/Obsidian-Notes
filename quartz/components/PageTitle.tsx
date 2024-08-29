9addd2698a0dc6fdb5e89395d487af99ce5f5cc3
Eduardo Blanco Bielsa <gitblanc@tutanota.com>
2024-08-29 11:42:49 +0200

    added logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,15 +6,24 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/logo.png")
   return (
-    <h1 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h1>
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+        <img class="Logo" src={iconPath} alt={title}/>
+      </a>
+    </h2>
   )
 }
 
 PageTitle.css = `
 .page-title {
+  font-size: 1.75rem;
+  margin: 0;
+}
+.Logo {
+  height: 40%;
+  width: 40%;
   margin: 0;
 }
 `
